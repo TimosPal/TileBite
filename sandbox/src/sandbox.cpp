be87da54a999b3d99121a0b51f7d9046fd7caf8b
TimosPal <timospale@gmail.com>
2025-04-17 13:10:29 +0300

    Added spdlog lib, added CMakeSettings for better IDE support and build config

--- sandbox/src/sandbox.cpp
+++ sandbox/src/sandbox.cpp
@@ -2,11 +2,12 @@
 
 #include <core/EntryPoint.hpp>
 #include <core/EngineApp.hpp>
+#include <utilities/Logger.hpp>
 
 class MyApp : public Engine::EngineApp {
 	void setup() override
 	{
-
+		Engine::LOG_INFO("Hi!");
 	}
 };
 
